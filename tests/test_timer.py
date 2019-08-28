ff9bc8fa36424d01b7953b5c3267f054103ac633
Viktor Stagge <viktorstagge@gmail.com>
2019-08-28 22:56:57 +0200

    feat: main functionality and summary works as expected. added unittests

---
+++ tests/test_timer.py
@@ -0,0 +1,89 @@
+import unittest
+
+from time import sleep
+from datetime import datetime
+from datetime import timedelta
+
+from timer import timer
+from timer import Timer
+from timer import Checkpoint
+
+
+class TestTimerMethods(unittest.TestCase):
+
+    def test_import(self):
+        from timer import timer
+        self.assertEqual(type(timer), Timer)
+
+    def test_call_doesnt_crash(self):
+        for _ in range(10):
+            actual = timer()
+            sleep(0.05)
+            print(actual)
+
+    def test_restart(self):
+        sleep(1)
+        timer.restart()
+        self.assertTrue(len(timer._checkpoints) == 1)
+        self.assertTrue(timer._start_checkpoint == timer._current_checkpoint)
+        self.assertTrue(timer._start_checkpoint.duration() < timedelta(milliseconds=500))
+
+    def test_one_checkpoint(self):
+        _start_checkpoint_before = timer._start_checkpoint
+        _current_checkpoint_before = timer._current_checkpoint
+        number_of_checkpoints_before = len(timer._checkpoints)
+
+        print(f'{self.test_one_checkpoint.__name__}\n')
+        for _ in range(3):
+            print(timer())
+            sleep(0.1)
+
+        checkpoint = timer.new_checkpoint(name='checkpoint_0')
+
+        for _ in range(3):
+            print(timer())
+            sleep(0.1)
+
+        _start_checkpoint_after = timer._start_checkpoint
+        _current_checkpoint_after = timer._current_checkpoint
+        number_of_checkpoints_after = len(timer._checkpoints)
+
+        self.assertEqual(number_of_checkpoints_before, 1, f'expected 1, received {number_of_checkpoints_before} checkpoints')
+        self.assertEqual(number_of_checkpoints_after, 2, f'expected 2, received {number_of_checkpoints_after} checkpoints')
+        self.assertTrue(_start_checkpoint_before == _start_checkpoint_after, '_start_checkpoint changed')
+        self.assertTrue(_start_checkpoint_before == _current_checkpoint_before, 'mismatch between first checkpoints')
+        self.assertTrue(_start_checkpoint_after != _current_checkpoint_after, 'current checkpoint or start checkpoint changed unexpectedly')
+        self.assertEqual(type(checkpoint), Checkpoint, '.new_checkpoint() does not return the Checkpoint')
+
+    def test_many_checkpoints(self):
+        pass
+
+    def test_string_representation(self):
+        pass
+
+    def test_summary(self):
+        timer.restart()
+
+        [timer() for _ in range(3)]
+        timer.new_checkpoint(name='checkpoint_0')
+        for _ in range(3):
+            timer()
+            sleep(0.1)
+
+        timer.new_checkpoint(name='checkpoint_1')
+        for _ in range(3):
+            timer()
+            sleep(0.1)
+
+        timer.new_checkpoint(name='victory lap')
+        for _ in range(3):
+            timer()
+            sleep(0.4)
+
+        summary = timer.summary()
+        print(f'{self.test_summary.__name__}\n')
+        print(summary)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- timer.py
+++ timer.py
@@ -1,118 +1,140 @@
 from datetime import datetime
 
 
-class Timer():
-	_timer = None
-	_name = None
-	_start_time = None
-	_current_checkpoint = 
-	_checkpoints = []
- 
-
-	def __new__(cls, *args, name='timer', output_format=str, **kwargs):
-		if cls._timer is not None:
-			cls._timer = super(Timer, cls).__new__(cls, *args, **kwargs)
-		cls._name = name
-		cls._format = output_format
-		cls._start_checkpoint = Checkpoint()
-		cls._current_checkpoint = cls._start_checkpoint
-		cls._checkpoints.append(cls._start_checkpoint)
-		return cls._timer
+_first_checkpoint_name = 'start'
 
 
-	def __call__(self, name=None):
-		now = datetime.now() 
-		time_since_start = self._time_since_start(timestamp=now)
-		if self._start_time != self._current_checkpoint.start:
-			time_since_checkpoint = self._time_since_checkpoint(timestamp=now)
-		else:
-			time_since_checkpoint = None
+class Timer:
+    _checkpoints = []
 
-		time = _describe_time(time_since_start=time_since_start, 
-							  time_since_checkpoint=time_since_checkpoint)
-		return time
+    @classmethod
+    def __new__(cls, *args, name='timer', output_format=str, **kwargs):
+        if not hasattr(cls, '_timer') or cls._timer is None:
+            cls._timer = super(Timer, cls).__new__(cls)
+
+            cls._name = name
+            cls._format = output_format
+            cls._start_checkpoint = Checkpoint(name=_first_checkpoint_name)
+            cls._current_checkpoint = cls._start_checkpoint
+            cls._checkpoints.append(cls._start_checkpoint)
+        return cls._timer
+
+    def __call__(self, description=None):
+        now = datetime.now()
+        time_since_start = self._time_since_start(timestamp=now)
+        if self._start_checkpoint != self._current_checkpoint:
+            time_since_checkpoint = self._time_since_checkpoint(timestamp=now)
+        else:
+            time_since_checkpoint = None
+
+        time = self._describe_time(time_since_start=time_since_start,
+                                   time_since_checkpoint=time_since_checkpoint,
+                                   description=description)
+        return time
+
+    @classmethod
+    def _describe_time(cls, time_since_start=None, time_since_checkpoint=None, description=None):
+        if time_since_start is None:
+            time_since_start = cls._time_since_start()
+
+        if cls._format == str:
+            return cls._describe_time_as_string(time_since_start=time_since_start,
+                                                time_since_checkpoint=time_since_checkpoint,
+                                                description=description)
+        elif cls._format == int:
+            return cls._describe_time_as_int(time_since_start=time_since_start)
+        else:
+            raise NotImplementedError()
+
+    @classmethod
+    def _describe_time_as_string(cls, time_since_start=None, time_since_checkpoint=None, description=None):
+        if description is None:
+            description = ''
+
+        time_string = f'{cls._name}:\t{time_since_start}'
+        if time_since_checkpoint is not None:
+            checkpoint_name_string = f'{cls._current_checkpoint.name + ":":<15}\t' if cls._current_checkpoint.name else ''
+            time_string += f'.\t\t{checkpoint_name_string}{time_since_checkpoint}\t{description}'
+        return time_string
+
+    @classmethod
+    def _describe_time_as_int(cls, time_since_start):
+        return time_since_start
+
+    @classmethod
+    def _time_since_start(cls, timestamp=None):
+        if timestamp is None:
+            timestamp = datetime.now()
+
+        return timestamp - cls._start_checkpoint.start
+
+    @classmethod
+    def _time_since_checkpoint(cls, timestamp=None):
+        if timestamp is None:
+            timestamp = datetime.now()
+
+        return timestamp - cls._current_checkpoint.start
+
+    @classmethod
+    def new_checkpoint(cls, name=None):
+        cls._current_checkpoint.end_checkpoint()
+        checkpoint = Checkpoint(name=name)
+        cls._current_checkpoint = checkpoint
+        cls._checkpoints.append(checkpoint)
+        return checkpoint
+
+    @classmethod
+    def duration(cls):
+        start_time = cls._checkpoints[0].start
+        end_time = cls._checkpoints[-1].end or datetime.now()
+        return end_time - start_time
+
+    @classmethod
+    def set_unit(cls, unit=str):
+        pass
+
+    @classmethod
+    def summary(cls):
+        longest_checkpoint_name = max((len(c.name) for c in cls._checkpoints))
+
+        _summary = f'{cls._name} summary\n'
+        for checkpoint in cls._checkpoints:
+            _summary += f'{checkpoint.name + ": " if checkpoint.name is not None else "":{longest_checkpoint_name + 2}}'
+            _summary += f'{cls._time_since_start(timestamp=checkpoint.start).total_seconds():4.3f}s\t'
+            _summary += f'duration={checkpoint.duration().total_seconds():4.3f}s\n'
+        _summary += f'{"end:":{longest_checkpoint_name + 2}}{cls.duration().total_seconds():4.3f}s\n'
+
+        return _summary
+
+    @classmethod
+    def restart(cls):
+        cls._start_checkpoint = Checkpoint(name=_first_checkpoint_name)
+        cls._current_checkpoint = cls._start_checkpoint
+        cls._checkpoints = [cls._start_checkpoint]
+        return cls._timer
 
 
-	def _describe_time(cls, time_since_start=None, time_since_checkpoint=None):
-		if time_since_start is None:
-			time_since_start = _time_since_start()
+class Checkpoint:
+    def __init__(self, name=None, start=None):
+        self.start = start or datetime.now()
+        self.end = None
+        self.name = name
 
-		if cls._format == str:
-			return _describe_time_as_string(time_since_start=time_since_start, time_since_checkpoint=time_since_checkpoint)
-		elif cls._format == int:
-			return _describe_time_as_int(time_since_start=time_since_start)
-		else:
-			raise NotImplementedError()
+    def __eq__(self, other):
+        return type(other) == type(self) and \
+               other.start == self.start and \
+               other.end == self.end and \
+               other.name == self.name
 
+    def duration(self):
+        if self.end is None:
+            return datetime.now() - self.start
 
-	def _describe_time_as_string(cls, time_since_start=None, time_since_checkpoint=None):
-		time_string = f'{cls._name}:\t{time_since_start}'
-		if time_since_checkpoint is not None:
-			checkpoint_name_string = f'{cls._current_checkpoint.name:15}:' if cls._current_checkpoint.name else ''  
-			time_string += f'.\t\t{checkpoint_name_string}{time_since_checkpoint}'
-		return time_string
+        return self.end - self.start
 
-
-	def _describe_time_as_int(cls, time_since_start):
-		return time_since_start
-
-
-	def _time_since_start(cls, timestamp=None):
-		if timestamp is None:
-			timestamp = datetime.now()
-
-		return now - cls._start_checkpoint.start
-
-
-	def _time_since_checkpoint(timestamp=None):
-		if timestamp is None:
-			timestamp = datetime.now()
-
-		return timestamp - cls._current_checkpoint.start
-
-
-	def new_checkpoint(cls, name=None):
-		checkpoint = Checkpoint(name=name)
-		cls._current_checkpoint = checkpoint
-		cls._checkpoints.append(checkpoint)
-		return checkpoint
-
-
-	def set_unit(cls, type=str):
-		pass
-
-
-	def summary(cls):
-		_summary = f'{name}\n'
-		for checkpoint in cls._checkpoints:
-			_summary += f'{cls._time_since_start(timestamp=checkpoint.start)}\t'
-			_summary += f'{checkpoint.name + ": " if checkpoint.name is not None else ""}'
-			_summary += f'{checkpoint.duration()}\n'
-		_summary = f'end: {_time_since_start()}'
-		return _summary
-
-	def restart(cls):
-		cls._start_checkpoint = Checkpoint()
-		cls._current_checkpoint = cls._start_checkpoint
-		cls._checkpoints.append(cls._start_checkpoint)
-		return self._timer
-
-
-class Checkpoint
-	start = None
-	end = None
-	name = None
-
-
-	def __init__(self, name=name, start=None):
-		self.start = start or datetime.now()
-		self.name = name
-
-	def duration(self):
-		if self.end is None:
-			return datetime.now() - self.start
-
-		return self.end - self.start
+    def end_checkpoint(self):
+        if self.end is None:
+            self.end = datetime.now()
 
 
 timer = Timer()
